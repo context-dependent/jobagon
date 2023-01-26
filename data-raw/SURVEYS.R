94cbcf2ce7382ad4335802b6a5f5bf75accd2e3c
tm <tomas.k.mcmanus@gmail.com>
2023-01-26 11:39:28 -0500

    Initial commit

---
+++ data-raw/SURVEYS.R
@@ -0,0 +1,9 @@
+## code prepares the SURVEYS dataset
+
+# Extract Survey Dataset Structure
+
+# Build Simulated Responses
+
+# Write Survey Datasets to Package
+
+usethis::use_data(SURVEYS, overwrite = TRUE)
